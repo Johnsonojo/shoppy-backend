6e27312ef5095158f7adf3bc7a76600865dd7b53
Johnsonojo <johnsonojo@gmail.com>
2023-11-17 22:00:00 +0100

    update shoppingList schema

--- src/db/models/ShoppingList.ts
+++ src/db/models/ShoppingList.ts
@@ -11,23 +11,36 @@ interface IShoppingListModel extends IShoppingList, Document {}
 
 const shoppingListSchema = new Schema(
   {
-    title: { type: String, required: true, unique: true },
+    title: {
+      type: String,
+      unique: true,
+      required: [true, "Please provide a title for your shopping list"],
+    },
     description: {
       type: String,
-      required: true,
       default: "No description",
       unique: true,
+      required: [true, "Please provide a description"],
     },
-    category: { type: String, required: true },
+    category: { type: String, required: [true, "Please provide a category"] },
     user: { type: Schema.Types.ObjectId, ref: "User", required: true },
     items: [
       {
-        name: { type: String, required: true },
-        quantity: { type: Number, default: 1 },
-        unitPrice: { type: Number, default: 0 },
+        name: {
+          type: String,
+          required: [true, "Please provide the item's name"],
+        },
+        quantity: {
+          type: Number,
+          required: [true, "Please provide the item's quantity"],
+        },
+        unitPrice: {
+          type: Number,
+          required: [true, "Please provide the item's unit price"],
+        },
       },
     ],
-    total: { type: Number, default: 0 },
+    total: { type: Number, required: true },
     isDone: { type: Boolean, default: false },
   },
   { timestamps: true }
